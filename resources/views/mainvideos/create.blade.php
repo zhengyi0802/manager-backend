8766718f9d0c6538d711326534b8a8edcfe705c0
Chevy Lin <chevylin0802@gmail.com>
2021-07-05 18:13:14 +0800

    update

--- resources/views/mainvideos/create.blade.php
+++ resources/views/mainvideos/create.blade.php
@@ -1,9 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', __('elearnings.title'))
+@section('title', __('mainvideos.title'))
 
 @section('content_header')
-    <h1 class="m-0 text-dark">{{ __('elearnings.header') }}</h1>
+    <h1 class="m-0 text-dark">{{ __('mainvideos.header') }}</h1>
 @stop
 
 @section('content')
@@ -13,7 +13,7 @@
             <h1>{{ __('tables.new') }}</h1>
         </div>
         <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('elearnings.index') }}">{{ __('tables.back') }}</a>
+            <a class="btn btn-primary" href="{{ route('mainvideos.index') }}">{{ __('tables.back') }}</a>
         </div>
     </div>
 </div>
@@ -29,70 +29,47 @@
     </div>
 @endif
 
-<form action="{{ route('elearnings.store') }}" method="POST" enctype="multipart/form-data" >
+<form action="{{ route('mainvideos.store') }}" method="POST" enctype="multipart/form-data" >
      @csrf
      <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div name="catagory_group" id="catagory_group" class="form-group">
-                <strong>{{ __('elearnings.catagory') }} : </strong>
-                <select id="catagory_id" name="catagory_id" >
-                    @foreach($elearningcatagories as $elearningcatagory)
-                       <option value="{{ $elearningcatagory->id }}">{{ $elearningcatagory->name }}</option>
+                <strong>{{ __('mainvideos.project') }} : </strong>
+                <select id="proj_id" name="proj_id" >
+                    @foreach($projects as $project)
+                       <option value="{{ $project->id }}">{{ $project->name }}</option>
                     @endforeach
                 </select>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>{{ __('elearnings.name') }} :</strong>
-                <input type="text" name="name" class="form-control">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>{{ __('elearnings.descriptions') }} :</strong>
-                <textarea class="form-control" style="height:150px" name="description"></textarea>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <div id="div-url-name"><strong>{{ __('elearnings.preview') }} : </strong></div>
-                <div id="div-image">
-                    <input type="file" id="image" name="image" accept="image/*" onchange="loadImage(event)" >
-                </div>
-                <div id="div-preview">
-                    <img name="preview" id="preview" >
-                </div>
-            </div>
-            <script>
-                    var loadImage = function(event) {
-                        var output = document.getElementById('preview');
-                        output.src = URL.createObjectURL(event.target.files[0]);
-                        output.onload = function() {
-                           URL.revokeObjectURL(output.src) // free memory
-                        }
-                    };
-            </script>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>{{ __('elearnings.mime_type') }} :</strong>
-                <select id="mime_type" name="mime_type" >
-                    <option value="video" selected>{{ __('elearnings.video') }}</option>
-                    <option value="youtube">{{ __('elearnings.youtube') }}</option>
-                    <option value="youtube_id">{{ __('elearnings.youtube_id') }}</option>
+                <strong>{{ __('mainvideos.type') }} :</strong>
+                <select id="type" name="type" >
+                     <option value="video" selected >{{ __('mainvideos.type_video') }}</option>
+                     <option value="playlist" >{{ __('mainvideos.type_playlist') }}</option>
+                     <option value="youtube_url" >{{ __('mainvideos.type_youtube_url') }}</option>
+                     <option value="youtube_id" >{{ __('mainvideos.type_youtube_id') }}</option>
+                     <option value="youtube_playlist" >{{ __('mainvideos.type_youtube_playlist') }}</option>
+                     <option value="youtube_playlist_id" >{{ __('mainvideos.type_youtube_playlist_id') }}</option>
                 </select>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>{{ __('elearnings.url') }} :</strong>
-                <input type="text" name="url" class="form-control">
+                <strong>{{ __('mainvideos.playlist') }} :</strong>
+                <textarea class="form-control" style="height:150px" name="playlist"></textarea>
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>{{ __('elearnings.status') }} :</strong>
+                <strong>{{ __('mainvideos.description') }} :</strong>
+                <textarea class="form-control" style="height:150px" name="description"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>{{ __('mainvideos.status') }} :</strong>
                 <input type="radio" name="status" value="1" checked>{{ __('tables.status_on') }}
                 <input type="radio" name="status" value="0">{{ __('tables.status_off') }}
             </div>

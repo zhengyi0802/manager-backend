81005d43f26f8c4edcc013d8bda7eed00c377a61
Chevy Lin <chevylin0802@gmail.com>
2023-05-15 10:54:08 +0800

    update

--- resources/lang/zh-TW/tables.php
+++ resources/lang/zh-TW/tables.php
@@ -20,7 +20,7 @@
            'yes'         => '是',
            'no'          => '否',
            'copylink'    => '複製鏈結',
-           'copylink_line' => '複製Linke專屬鏈結',
+           'copylink_line' => '複製Line專屬鏈結',
            'backend_url'                   => 'https://backend.mdo.tw/',
            'reseller_application_url'      => 'https://sales.mdo.tw/resellers/create?introducer=',
            'distrobuter_application_url'   => 'https://sales.mdo.tw/distrobuters/create?introducer=',
